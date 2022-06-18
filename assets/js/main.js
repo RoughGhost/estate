7252baf1e19eb720d7fcab3f52241861b8b992c9
RoughGhost <stephendlouis6@gmail.com>
2022-06-18 17:10:01 +0100

    updated section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,69 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEL: ".swiper-button-prev",
+  },
+});
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+    toggleItem(item);
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
